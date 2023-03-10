954e26e2925a2ad6481e22de39133f62a13e081b
DimitarZlatev10 <aguerofc@abv.bg>
2023-03-10 18:51:12 +0200

    created header footer and home section

--- JS,HTML,CSS-frontend/pages-script/home.js
+++ JS,HTML,CSS-frontend/pages-script/home.js
@@ -1,2 +1,22 @@
-console.log(window.location);
-const aa = "1";
+const menuBar = document.getElementById("menu-btn");
+const sideBarMenu = document.querySelector(".side-bar");
+const closeSideBarMenu = document.getElementById("close-side-bar");
+
+menuBar.onclick = () => {
+  sideBarMenu.style.left = "0";
+  sideBarMenu.style.boxShadow = "0 0 0 100vw rgba(0, 0, 0, 0.7)";
+};
+
+closeSideBarMenu.onclick = () => {
+  sideBarMenu.style.left = "-120%";
+  sideBarMenu.style.boxShadow = "";
+};
+
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
